6fda6802cd99d780ac7d5a735de818b620cfa6af
Bibash7 <biswash166@gmail.com>
2023-07-31 11:21:51 +0545

    adding form field in table

---
+++ src/main/java/com/testproj/pdfreader/CreateFormInTable.java
@@ -0,0 +1,94 @@
+package com.testproj.pdfreader;
+
+
+import com.itextpdf.forms.fields.TextFormFieldBuilder;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.forms.PdfAcroForm;
+import com.itextpdf.forms.fields.PdfTextFormField;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.properties.UnitValue;
+import com.itextpdf.layout.renderer.CellRenderer;
+import com.itextpdf.layout.renderer.DrawContext;
+import com.itextpdf.layout.renderer.IRenderer;
+
+import java.io.File;
+
+public class CreateFormInTable {
+    public static final String DEST = "./target/sandbox/acroforms/create_form_in_table.pdf";
+
+    public static void main(String[] args) throws Exception {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+
+        new CreateFormInTable().manipulatePdf(DEST);
+    }
+
+    protected void manipulatePdf(String dest) throws Exception {
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
+        Document doc = new Document(pdfDoc);
+
+        Table table = new Table(UnitValue.createPercentArray(2)).useAllAvailableWidth();
+
+        Cell cell = new Cell().add(new Paragraph("Name:"));
+        table.addCell(cell);
+
+        cell = new Cell();
+        cell.setNextRenderer(new CreateFormFieldRenderer(cell, "name"));
+        table.addCell(cell);
+
+        cell = new Cell().add(new Paragraph("Address"));
+        table.addCell(cell);
+
+        cell = new Cell();
+        cell.setNextRenderer(new CreateFormFieldRenderer(cell, "address"));
+        table.addCell(cell);
+
+        cell = new Cell().add(new Paragraph("Phone Number"));
+        table.addCell(cell);
+
+        cell = new Cell();
+        cell.setNextRenderer(new CreateFormFieldRenderer(cell, "phoneNumber"));
+        table.addCell(cell);
+
+
+        cell = new Cell().add(new Paragraph("Email"));
+        table.addCell(cell);
+
+        cell = new Cell();
+        cell.setNextRenderer(new CreateFormFieldRenderer(cell, "email"));
+        table.addCell(cell);
+
+        doc.add(table);
+
+        doc.close();
+    }
+
+
+    private class CreateFormFieldRenderer extends CellRenderer {
+        protected String fieldName;
+
+        public CreateFormFieldRenderer(Cell modelElement, String fieldName) {
+            super(modelElement);
+            this.fieldName = fieldName;
+        }
+        @Override
+        public IRenderer getNextRenderer() {
+            return new CreateFormFieldRenderer((Cell) modelElement, fieldName);
+        }
+
+        @Override
+        public void draw(DrawContext drawContext) {
+            super.draw(drawContext);
+
+            PdfTextFormField field = new TextFormFieldBuilder(drawContext.getDocument(), fieldName)
+                    .setWidgetRectangle(getOccupiedAreaBBox()).createText();
+            field.setValue("");
+            PdfAcroForm form = PdfAcroForm.getAcroForm(drawContext.getDocument(), true);
+            form.addField(field);
+        }
+    }
+}
