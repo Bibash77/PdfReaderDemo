e285c52ca299c31231c0a9acd906ceaf3281fac2
bibash <biswsh166@gmail.com>
2023-08-02 08:18:39 +0545

    page break

--- src/main/java/com/testproj/pdfreader/learnday1/LatestPdfLineTest.java
+++ src/main/java/com/testproj/pdfreader/learnday1/LatestPdfLineTest.java
@@ -1,13 +1,13 @@
-package com.testproj.pdfreader;
+package com.testproj.pdfreader.learnday1;
 
 
+import com.itextpdf.forms.PdfAcroForm;
+import com.itextpdf.forms.fields.PdfTextFormField;
 import com.itextpdf.forms.fields.TextFormFieldBuilder;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.forms.PdfAcroForm;
-import com.itextpdf.forms.fields.PdfTextFormField;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.AreaBreak;
 import com.itextpdf.layout.element.Cell;
@@ -21,64 +21,30 @@ import com.itextpdf.layout.renderer.IRenderer;
 
 import java.io.File;
 
-public class CreateFormInExistingPdf {
+public class LatestPdfLineTest {
     public static final String DEST = "./target/sandbox/acroforms/create_form_in_table.pdf";
-    public static final String SRC = "./src/main/resources/pdfs/form.pdf";
+    public static final String SRC = "./src/main/resources/pdfs/2page.pdf";
     public static void main(String[] args) throws Exception {
         File file = new File(DEST);
         file.getParentFile().mkdirs();
 
-        new CreateFormInExistingPdf().manipulatePdf(DEST);
+        new LatestPdfLineTest().manipulatePdf(DEST);
     }
 
     protected void manipulatePdf(String dest) throws Exception {
         PdfDocument pdfDoc = new PdfDocument(new PdfReader(SRC), new PdfWriter(dest));
-
         Document doc = new Document(pdfDoc);
+
+        // Create a new page
+        PageSize ps = new PageSize(pdfDoc.getFirstPage().getPageSize());
         pdfDoc.addNewPage();
-        doc.add(new AreaBreak(AreaBreakType.LAST_PAGE));
-        doc.add(new Paragraph("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
-                "\n"));
 
-        doc.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
-
+        // Go to the new page and add content
+        doc.add(new AreaBreak(AreaBreakType.LAST_PAGE)); // Go to the last added page
         doc.add(new Paragraph("Please Fill below form"));
-
-        Table table = new Table(UnitValue.createPercentArray(2)).useAllAvailableWidth();
-
-        Cell cell = new Cell().add(new Paragraph("Name:"));
-        table.addCell(cell);
-
-        cell = new Cell();
-        cell.setNextRenderer(new CreateFormFieldRenderer(cell, "name1"));
-        table.addCell(cell);
-
-        cell = new Cell().add(new Paragraph("Address"));
-        table.addCell(cell);
-
-        cell = new Cell();
-        cell.setNextRenderer(new CreateFormFieldRenderer(cell, "address"));
-        table.addCell(cell);
-
-        cell = new Cell().add(new Paragraph("Phone Number"));
-        table.addCell(cell);
-
-        cell = new Cell();
-        cell.setNextRenderer(new CreateFormFieldRenderer(cell, "phoneNumber"));
-        table.addCell(cell);
-
-
-        cell = new Cell().add(new Paragraph("Email"));
-        table.addCell(cell);
-
-        cell = new Cell();
-        cell.setNextRenderer(new CreateFormFieldRenderer(cell, "email"));
-        table.addCell(cell);
-
-        doc.add(table);
-
         doc.close();
     }
+
 
 
     private class CreateFormFieldRenderer extends CellRenderer {
